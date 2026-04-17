33d1b3439124b96a0230326a44a2ec8835827088
ksenia274 <kcen,yackubowa@2013@gmail.com>
2026-04-17 11:08:54 +0300

    Fix UI

--- app/src/main/java/com/example/studcampapp/model/User.kt
+++ app/src/main/java/com/example/studcampapp/model/User.kt
@@ -6,10 +6,10 @@ import kotlinx.serialization.Serializable
 data class User (
     val id: String,
     val login: String,
-    val firstName: String?,
-    val lastName: String?,
-    val middleName: String?,
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val middleName: String? = null,
     val avatarUrl: String? = null,
-    val phone: String?,
-    val email: String?
+    val phone: String? = null,
+    val email: String? = null
 )
